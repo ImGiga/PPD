df5faddc9941cd1efab8213928bb3c336ff63590
maxing <maxgiessuebel@icloud.com>
2024-02-27 11:17:03 +0100

    m

--- Versuche/Röntgenbeugung/Programme/meinplot.py
+++ Versuche/Röntgenbeugung/Programme/meinplot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import savgol_filter
 
-w_mess, I_mess, non = np.loadtxt('/Users/max/Documents/Uni Bayreuth/Master/Semester 1/PPD/Versuche/Röntgenbeugung/Daten/REFVORN.STN', skiprows=22, unpack=True)
+w_mess, I_mess, non = np.loadtxt('/Users/max/Documents/Uni Bayreuth/Master/Semester 1/PPD/Versuche/Röntgenbeugung/Daten/Messung 1/REFVORN.STN', skiprows=22, unpack=True)
 
 plt.plot(w_mess, I_mess, label='Messung')
 plt.show()
